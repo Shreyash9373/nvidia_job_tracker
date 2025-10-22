402d31b34d64e68e1193c5b89b2466f99dd0fe08
shreyas <shreyashraut8@gmail.com>
2025-10-23 00:45:31 +0530

    Added option for adding vercel url

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,7 +1,15 @@
 "use client";
 
 import axios from "axios";
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const baseURL =
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  (typeof window === "undefined"
+    ? process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}` // On Vercel server
+      : "http://localhost:3000" // Local dev server
+    : window.location.origin); // On browser
+
 export const api = axios.create({
   baseURL,
   withCredentials: true,
